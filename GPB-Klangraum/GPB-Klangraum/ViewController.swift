3eee7e7230f60af159a0e933289b9fa8985eaab0
pixelskull <pascal.schoenthier@googlemail.com>
2015-07-28 17:44:20 +0200

    initial commit
    created single page iOS Application

---
+++ GPB-Klangraum/GPB-Klangraum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPB-Klangraum
+//
+//  Created by pascal on 28/07/15.
+//  Copyright (c) 2015 Pascal Schönthier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
