4823033501325188aa7eae5fdd43c7f71a2d959e
pixelskull <pascal.schoenthier@googlemail.com>
2015-08-19 14:14:49 +0200

    basic FileService Implementation
    first version of FileServices implemented in KlangraumKit.framework

--- GPB-Klangraum/AppDelegate.swift
+++ GPB-Klangraum/AppDelegate.swift
@@ -12,6 +12,7 @@ import AudioToolbox
 import CoreAudio
 
 import AVFoundation
+
 import KlangraumKit
 
 @UIApplicationMain
@@ -21,146 +22,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
+        var audioFileService = AudioFileService()
 
-        /**
-            Create dummy-AudioFile
-        **/
-        let count:Int = 44100 * 3 // samplerate * seconds?!
-        let frequency:Float = 4.0
-//        let amplitude:Float = 3.0
+        var dummyPath:String? = audioFileService.createDummyFile()
+        println(dummyPath)
 
 
-//        var x:[Float] = map(0..<count){ 2.0 * Float(M_PI) / Float(count) * Float($0) * frequency }
 
-        var test:UnsafeMutablePointer<Void> = UnsafeMutablePointer<Void>(sin(map(0..<count){ 2.0 * Float(M_PI) / Float(count) * Float($0) * frequency }))
+//        var asset:AVURLAsset = AVURLAsset(URL: NSURL(string: dummyPath!), options: nil)
+////        var tracks:Array = asset.tracks
+//        var duration = asset.duration
+//        println("duration: \(CMTimeGetSeconds(duration))")
 
-        var buffer:AudioBufferList = AudioBufferList()
-        buffer.mNumberBuffers = 1
-        buffer.mBuffers.mNumberChannels = 1
-        buffer.mBuffers.mDataByteSize = UInt32(count)
-        buffer.mBuffers.mData = test
+        var buffer = audioFileService.readAudioFileToAudioBufferList(dummyPath!)
 
-        let newFilePath: String = NSBundle.mainBundle().resourcePath! + "/dummy.caf"
-//        let fileManager = NSFileManager.defaultManager()
-//        fileManager.createFileAtPath(newFilePath, contents: nil, attributes: nil)
+        let array = audioFileService.convertAudioBufferListToFloatArray(buffer!, length: 44100*3)
 
-        var newFileDesc: AudioStreamBasicDescription = AudioStreamBasicDescription()
-        newFileDesc.mSampleRate = 44100
-        newFileDesc.mFormatID = AudioFormatID(kAudioFormatLinearPCM)
-        newFileDesc.mFormatFlags = AudioFormatFlags(kAudioFormatFlagIsFloat)
-        newFileDesc.mChannelsPerFrame = 1
-        newFileDesc.mBitsPerChannel = 32
-        newFileDesc.mBytesPerFrame = 4
-        newFileDesc.mFramesPerPacket = 1
-        newFileDesc.mBytesPerPacket = newFileDesc.mFramesPerPacket * newFileDesc.mBytesPerFrame
+        var result = array!.filter{ i in !i.isNaN && i != 0.0 }
+        println(array)
 
-        var urlRef:NSURL = NSURL(string: newFilePath)!
-
-        var extAudio: ExtAudioFileRef = ExtAudioFileRef()
-        ExtAudioFileCreateWithURL(urlRef, AudioFileTypeID(kAudioFileCAFType), &newFileDesc, nil, UInt32(kAudioFileFlags_EraseFile), &extAudio)
-
-        var wErr = noErr
-
-        wErr = ExtAudioFileWrite(extAudio, UInt32(count), &buffer)
-
-
-        /*
-            ExtAudioFile
-        */
-        println("start reading file")
-        var err:OSStatus = noErr
-
-//        let filePath:String = NSBundle.mainBundle().resourcePath!+"/YellowNintendoHero-Muciojad.mp3"
-//        let filePath:String = NSBundle.mainBundle().resourcePath!+"/Test.m4a"
-        let filePath:String = newFilePath
-
-        let audioURL: NSURL = NSURL(string: filePath)!
-
-        var audioFile: AudioFileID = nil
-        var hint: AudioFileTypeID = 0
-
-        err = AudioFileOpenURL(audioURL, Int8(kAudioFileReadPermission), hint, &audioFile)
-        println(err)
-
-        if (err == noErr) {
-
-            // reading file-properties
-            var outDataSize: UInt32 = 0
-            var isWritable: UInt32 = 0
-            err = AudioFileGetPropertyInfo(audioFile, UInt32(kAudioFilePropertyInfoDictionary), &outDataSize, &isWritable)
-            println(err)
-            println(outDataSize)
-
-            var dict:NSDictionary?
-            var key:AudioChannelLayout = AudioChannelLayout()
-            err = AudioFileGetProperty(audioFile , UInt32(kAudioFilePropertyInfoDictionary), &outDataSize, &key)
-
-            println(err)
-            println("Dictionary: \(key.mChannelBitmap)")
-
-            err = noErr
-
-            // open audiofile
-            var audioFileRef: ExtAudioFileRef = ExtAudioFileRef()
-            err = ExtAudioFileOpenURL(audioURL, &audioFileRef)
-
-            println(err)
-
-            // optional set Audio Encoding to linearPCM dual-channel with Float-values
-//            var dstForm:AudioStreamBasicDescription = AudioStreamBasicDescription()
-//            dstForm.mSampleRate = 44100
-//            dstForm.mFormatID = AudioFormatID(kAudioFormatLinearPCM)
-//            dstForm.mFormatFlags = AudioFormatFlags(kAudioFormatFlagIsFloat)
-//            dstForm.mBitsPerChannel = 32
-//            dstForm.mChannelsPerFrame = 2
-//            dstForm.mBytesPerFrame = dstForm.mChannelsPerFrame * 2
-//            dstForm.mFramesPerPacket = 1
-//            dstForm.mBytesPerPacket = dstForm.mFramesPerPacket * dstForm.mBytesPerFrame
-//
-//            err = noErr
-//            err = ExtAudioFileSetProperty(audioFileRef, ExtAudioFilePropertyID(kExtAudioFileProperty_ClientDataFormat), UInt32(sizeof(AudioStreamBasicDescription)), &dstForm)
-
-            var numberOfFrames:UInt32 = UInt32(count) // 512
-            var data:NSMutableData = NSMutableData(length: Int(numberOfFrames)*sizeof(Float32))!
-
-            // prepare AudioBufferlist
-            var audioBufferList: AudioBufferList = AudioBufferList()
-            audioBufferList.mNumberBuffers = 1
-            audioBufferList.mBuffers.mData = data.mutableBytes
-            audioBufferList.mBuffers.mDataByteSize = UInt32(data.length)
-            audioBufferList.mBuffers.mNumberChannels = 2
-
-            // read File
-            do {
-                if numberOfFrames <= UInt32(data.length / sizeof(Float32)) {
-                    println("numberOfFrames <= \(data.length / sizeof(Float32)) \(numberOfFrames)")
-                } else {
-                    println("do something with Readdata ....")
-                }
-
-                println("numberOfFrames: \(numberOfFrames)")
-                err = ExtAudioFileRead(audioFileRef, &numberOfFrames, &audioBufferList)
-
-                if err != noErr {
-                    break
-                }
-            } while numberOfFrames != 0
+//        println(result)
 
 //            println(data)
-
 
             /**
                 Converting Data to Float Array
             **/
-            var count = data.length / sizeof(Float32)
-            var array = [Float32](count: count, repeatedValue: 0.0)
-            data.getBytes(&array, length: data.length)
 //            println(array)
             // remove zero-values
 
-        } else {
-            println("error: \(err)")
-        }
 
         return true 
     }
